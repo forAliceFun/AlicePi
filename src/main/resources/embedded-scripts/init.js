3413d1b95da2d4098a4105aadcb93f4279e94618
Tony <sinoxutong@live.cn>
2017-12-28 17:35:48 +0800

    发布消息

--- src/main/resources/embedded-scripts/init.js
+++ src/main/resources/embedded-scripts/init.js
@@ -5,9 +5,15 @@ console.log("console.log enable");
 
 //---------
 var MqSub = Java.type("fun.forAlice.AlicePi.core.pubsub.impl.SubscriberRedisImpl");
+var MqPub = Java.type("fun.forAlice.AlicePi.core.pubsub.impl.PublisherRedisImpl");
 
 var sub = new MqSub();
+var pub = new MqPub();
 
 sub.on("/gpios","1",function(msg){
 	print(msg)
 })
+
+window.setInterval(function(){
+	pub.publish("/gpios","fdaf");
+},1000*5);
