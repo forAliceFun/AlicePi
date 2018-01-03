3df32f7e4f5409a0cd009d0f4c5f9057aa362797
Tony <sinoxutong@live.cn>
2018-01-03 17:50:52 +0800

    定制任务增加
    setInterval
    clearInterval
    setTimeout

--- src/main/resources/embedded-scripts/init.js
+++ src/main/resources/embedded-scripts/init.js
@@ -2,10 +2,14 @@ print("engine init");
 var console={"log":print};
 console.log("console.log enable");
 
+var Thread = Java.type("java.lang.Thread");
+var Executors = Java.type("java.util.concurrent.Executors");
+var TimeUnit = Java.type("java.util.concurrent.TimeUnit");
 
 //---------
 var MqSub = Java.type("fun.forAlice.AlicePi.core.pubsub.impl.SubscriberRedisImpl");
 var MqPub = Java.type("fun.forAlice.AlicePi.core.pubsub.impl.PublisherRedisImpl");
+
 
 var sub = new MqSub();
 var pub = new MqPub();
@@ -14,6 +18,64 @@ sub.on("/gpios","1",function(msg){
 	print(msg)
 })
 
-window.setInterval(function(){
-	pub.publish("/gpios","fdaf");
-},1000*5);
+/****
+ 阻塞执行
+ 
+ //block program
+var r = new java.lang.Runnable() {
+    run: function() { 
+    	Thread.sleep(10*1000);
+    	pub.publish("/gpios","fdaf");
+    	print("run"); }
+}
+ 
+r.run();
+**///
+
+var scheduledThreadPool = Executors.newScheduledThreadPool(5);
+var r = new java.lang.Runnable() {
+    run: function() { 
+    	print()
+    	Thread.sleep(10*1000);
+    	pub.publish("/gpios","fdaf");
+    	print("run"); }
+};
+function setInterval(fun, ms){
+	var r = new java.lang.Runnable() {
+	    run: fun
+	};
+	return scheduledThreadPool
+		.scheduleAtFixedRate(
+					r,
+					0,//initialDelay
+					ms,
+					TimeUnit.MILLISECONDS
+				);
+}
+function clearInterval(future){
+	future.cancel(true);
+}
+function setTimeout(){
+	scheduledThreadPool
+	.schedule(
+				r,
+				ms,
+				TimeUnit.MILLISECONDS
+			);
+}
+
+/*** test demo
+var i = 0;
+var task = 
+setInterval(function(){
+	print("setInterval")
+	print(i++);
+	if(i>10){
+		clearInterval(task);
+	}
+},1000*2)
+while(true){
+	
+}
+
+**/
